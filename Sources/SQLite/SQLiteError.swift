fa4703df494837a704cb707668155180be05477b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-02-05 11:37:29 +0500

    Create SQLiteError.Reason to manage all custom errors in one place

--- Sources/SQLite/SQLiteError.swift
+++ Sources/SQLite/SQLiteError.swift
@@ -10,3 +10,21 @@ public struct SQLiteError: CustomStringConvertible, Error {
         self.reason = reason ?? Reason.unknown.message
     }
 }
+
+extension SQLiteError {
+    enum Reason {
+        case invalidColumnValue
+        case unexpectedNilColumnName
+        case unexpectedNilColumnValue
+        case unknown
+
+        var message: String {
+            switch self {
+            case .invalidColumnValue: return "Invalid column value"
+            case .unexpectedNilColumnName: return "Unexpected nil column name"
+            case .unexpectedNilColumnValue: return "Unexpected nil column value"
+            case .unknown: return "Unknown error"
+            }
+        }
+    }
+}
