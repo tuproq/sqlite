dc8b74823816d2473ce0023a08b61af386676421
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-02-05 11:45:09 +0500

    Conform String to Codable

---
+++ Sources/SQLite/Codable/String.swift
@@ -0,0 +1,16 @@
+import SQLite3
+
+extension String: Codable {
+    public init(statement: Statement, column: Int32) throws {
+        if let cString = sqlite3_column_text(statement.handle, column) {
+            self = .init(cString: cString)
+        } else {
+            throw error(reason: .unexpectedNilColumnValue)
+        }
+    }
+
+    public func encode(into statement: Statement, column: Int32) throws {
+        let code = sqlite3_bind_text(statement.handle, column, self, Int32(utf8.count), SQLITE_TRANSIENT)
+        try assert(handle: statement.handle, code: code)
+    }
+}
