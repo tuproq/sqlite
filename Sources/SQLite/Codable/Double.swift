8b798088e72205287124214ac88aa1a9f3c522e5
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-02-05 11:44:43 +0500

    Conform Double to Codable

---
+++ Sources/SQLite/Codable/Double.swift
@@ -0,0 +1,12 @@
+import SQLite3
+
+extension Double: Codable {
+    public init(statement: Statement, column: Int32) throws {
+        self = .init(sqlite3_column_double(statement.handle, column))
+    }
+
+    public func encode(into statement: Statement, column: Int32) throws {
+        let code = sqlite3_bind_double(statement.handle, column, self)
+        try assert(handle: statement.handle, code: code)
+    }
+}
