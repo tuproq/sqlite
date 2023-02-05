1082b0cd7910fdcbc29cb5ae8faa155b1db31889
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-02-05 11:45:22 +0500

    Conform Int to Codable

---
+++ Sources/SQLite/Codable/Int.swift
@@ -0,0 +1,23 @@
+import SQLite3
+
+extension FixedWidthInteger {
+    public init(statement: Statement, column: Int32) throws {
+        self = numericCast(sqlite3_column_int64(statement.handle, column))
+    }
+
+    public func encode(into statement: Statement, column: Int32) throws {
+        let code = sqlite3_bind_int64(statement.handle, column, Int64(self))
+        try assert(handle: statement.handle, code: code)
+    }
+}
+
+extension Int: Codable {}
+extension Int8: Codable {}
+extension Int16: Codable {}
+extension Int32: Codable {}
+extension Int64: Codable {}
+extension UInt: Codable {}
+extension UInt8: Codable {}
+extension UInt16: Codable {}
+extension UInt32: Codable {}
+extension UInt64: Codable {}
