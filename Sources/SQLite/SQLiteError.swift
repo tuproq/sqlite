bddc370e2a03bf586208adaec4b89ed1f580f370
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-02-05 11:36:35 +0500

    Refactor SQLiteError

--- Sources/SQLite/SQLiteError.swift
+++ Sources/SQLite/SQLiteError.swift
@@ -1,5 +1,12 @@
+import SQLite3
+
 public struct SQLiteError: CustomStringConvertible, Error {
     public let code: Int32
     public let reason: String
-    public var description: String { "Code: \(code), Reason: \(reason)" }
+    public var description: String { "[\(code)] \(reason)" }
+
+    init(code: Int32, reason: String? = nil) {
+        self.code = code
+        self.reason = reason ?? Reason.unknown.message
+    }
 }
